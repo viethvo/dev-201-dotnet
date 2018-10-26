4484edf786c1f40601f2f4750f07694f1abd2e11
viet.vo <viethvo@kms-technology.com>
2018-10-26 23:23:54 +0700

    develop | Add CsvHelper and create Read vs Write functionalities

---
+++ KMS.Next.CodeQuality/CSV/CsvHelper.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMS.Next.CodeQuality.CSV
+{
+    /// <summary>
+    /// Class CsvHelper.
+    /// </summary>
+    public class CsvHelper
+    {
+        /// <summary>
+        /// Reads catefory from csv file.
+        /// </summary>
+        /// <param name = "path">Path of category csv file.</param>
+        /// <return>List of T</return>
+        public static List<T> ReadFromFile<T>(string path)
+        {
+            // Check path
+            if (string.IsNullOrEmpty(path))
+            {
+                return null;
+            }
+
+            // Check type
+            if (typeof(T) != typeof(Category) && typeof(T) != typeof(Product))
+            {
+                throw new Exception("This type is not supported!");
+            }
+
+            // Read all from file
+            var lines = ReadAllLines(path);
+
+            // Convert lines to list
+            var resultList = ConvertToList<T>(lines);
+            return resultList;
+        }
+
+        /// <summary>
+        /// Converts content of csv file to list.
+        /// </summary>
+        /// <param name = "lines">Content of csv file.</param>
+        /// <return>List of T</return>
+        private static List<T> ConvertToList<T>(Task<string[]> lines)
+        {
+            bool isHeader = true;
+            var propListT = typeof(T).GetProperties();
+            var listResult = new List<T>();
+
+            foreach (string line in lines.Result)
+            {
+                // Skip header
+                if (isHeader)
+                {
+                    isHeader = false;
+                    continue;
+                }
+
+                // Get values of line
+                var values = line.Split(',');
+
+                // Check if values match properties
+                if (values.Length != propListT.Length)
+                {
+                    listResult.Add(default(T));
+                    continue;
+                }
+
+                var item = Activator.CreateInstance(typeof(T));
+                try
+                {
+                    // Add values to class if match
+                    for (int i = 0; i < propListT.Length; i++)
+                    {
+                        if (string.IsNullOrEmpty(values[i]))
+                        {
+                            continue;
+                        }
+                        // Check if nullable and match
+                        Type type = Nullable.GetUnderlyingType(propListT[i].PropertyType) ?? propListT[i].PropertyType;
+                        object safeValue = (values[i] == null) ? null : Convert.ChangeType(values[i], type);
+                        propListT[i].SetValue(item, safeValue, null);
+                    }
+                }
+                catch
+                {
+                    // Can't convert type
+                    listResult.Add(default(T));
+                    continue;
+                }
+
+                // Convert type success and add to list
+                var itemConvert = (T)Convert.ChangeType(item, typeof(T));
+                listResult.Add(itemConvert);
+            }
+            return listResult;
+        }
+
+        /// <summary>
+        /// Reads content of csv file.
+        /// </summary>
+        /// <param name = "path">Path of category csv file.</param>
+        /// <return>List of T</return>
+        private static async Task<string[]> ReadAllLines(string path)
+        {
+            StreamReader reader = null;
+            string fileText = string.Empty;
+            try
+            {
+                reader = File.OpenText(path);
+                fileText = await reader.ReadToEndAsync();
+            }
+            catch
+            {
+                throw new Exception("Failed to read file");
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+            }
+
+            return fileText.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+        }
+    }
+
+    /// <summary>
+    /// Class Product.
+    /// </summary>
+    public class Product
+    {
+        /// <summary>
+        /// Gets or sets identifier of product.
+        /// </summary>
+        /// <value>The identifier of product.</value>
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// Gets or sets name of product.
+        /// </summary>
+        /// <value>The name of product.</value>
+        public string ProductName { get; set; }
+
+        /// <summary>
+        /// Gets or sets price of product.
+        /// </summary>
+        /// <value>The price of product.</value>
+        public double Price { get; set; }
+
+        /// <summary>
+        /// Gets or sets description of product.
+        /// </summary>
+        /// <value>The description of product.</value>
+        public string ProductDescription { get; set; }
+
+        /// <summary>
+        /// Gets or sets expired date of product.
+        /// </summary>
+        /// <value>The expired date of product.</value>
+        public DateTime ExpiredDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets category identifier of product.
+        /// </summary>
+        /// <value>The category identifier of product.</value>
+        public int? CategoryId { get; set; }
+
+        /// <summary>
+        /// Gets or sets deleted flag.
+        /// </summary>
+        /// <value>The deleted flag.</value>
+        public bool DeletedFlag { get; set; }
+
+        /// <summary>
+        /// Gets and convert Product class to string.
+        /// </summary>
+        /// <return>System.String.</return>
+        public override string ToString()
+        {
+            return string.Format("{0},{1},{2},{3},{4},{5},{6}", ProductId, ProductName, Price, ProductDescription, ExpiredDate, CategoryId, DeletedFlag);
+        }
+    }
+
+    /// <summary>
+    /// Class Category.
+    /// </summary>
+    public class Category
+    {
+        /// <summary>
+        /// Gets or sets identifier of category .
+        /// </summary>
+        /// <value>The identifier of category.</value>
+        public int CategoryId { get; set; }
+
+        /// <summary>
+        /// Gets or sets name of category .
+        /// </summary>
+        /// <value>The name of category.</value>
+        public string CategoryName { get; set; }
+
+        /// <summary>
+        /// Gets or sets description of category .
+        /// </summary>
+        /// <value>The description of category.</value>
+        public string CategoryDescription { get; set; }
+
+        /// <summary>
+        /// Gets or sets deleted flag.
+        /// </summary>
+        /// <value>The deleted flag.</value>
+        public bool DeletedFlag { get; set; }
+
+        /// <summary>
+        /// Gets and convert Product class to string.
+        /// </summary>
+        /// <return>System.String.</return>
+        public override string ToString()
+        {
+            return string.Format("{0},{1},{2},{3}", CategoryId, CategoryName, CategoryDescription, DeletedFlag);
+        }
+    }
+
+    /// <summary>
+    /// Class ListExtension.
+    /// </summary>
+    public static class ListExtension
+    {
+        /// <summary>
+        /// Prints all content from list.
+        /// </summary>
+        /// <param name = "resultList" > The list.</param>
+        public static void PrintAll<T>(this List<T> resultList)
+        {
+            // Check type
+            if (typeof(T) != typeof(Product) && typeof(T) != typeof(Category))
+            {
+                throw new Exception("This type is not supported!");
+            }
+
+            // Get properties of Category Class
+            var properties = typeof(T).GetProperties();
+
+            // Print title
+            foreach (var property in properties)
+            {
+                Console.Write("{0}\t", property.Name);
+            }
+
+            Console.WriteLine();
+
+            // Print content
+            for (int i = 0; i < resultList.Count; i++)
+            {
+                if (resultList[i] != null)
+                {
+                    foreach (var property in properties)
+                    {
+                        Console.Write("{0}\t", property.GetValue(resultList[i]));
+                    }
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("Line {0}: Invalid format", i + 1);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Writes content to csv file.
+        /// </summary>
+        /// <param name = "resultList" >The result list.</param>
+        /// <param name = "path" >The path of csv file.</param>
+        /// <return>Task.</return>
+        public static async Task WriteToFile<T>(this List<T> resultList, string path)
+        {
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+
+            string content = string.Empty;
+
+            // Get properties
+            var properties = typeof(T).GetProperties();
+
+            // Write file
+            var writer = File.OpenWrite(path);
+            var streamWriter = new StreamWriter(writer);
+
+            foreach (var item in resultList)
+            {
+                await streamWriter.WriteLineAsync(item.ToString());
+            }
+
+            // Close stream
+            streamWriter.Close();
+            writer.Close();
+        }
+    }
+}
--- KMS.Next.CodeQuality/Program.cs
+++ KMS.Next.CodeQuality/Program.cs
@@ -1,10 +1,6 @@
-﻿using System;
+﻿using KMS.Next.CodeQuality.CSV;
+using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 /// <summary>
 /// This file contains a lot of code smells.
 /// Todo: Refactor the code to make it clean
@@ -15,57 +11,20 @@ namespace KMS.Next.CodeQuality
     {
         static void Main(string[] args)
         {
-            // Read product 
-            var lines = File.ReadLines("Data//product.csv");
-            List<Product> products = new List<Product>();
-            var isHeader = false;
-            var lists = typeof(Product).GetProperties();
-            string[] line01 = null;
-            foreach (var line in lines)
-            {
-                // Todo: Need to fix. The below line of code will cause exception 
-                // whenever a field value contains comma (,) character
-                string[] arr = line.Split(',');
-                if(!isHeader)
-                {
-                    foreach (var item in lists)
-                    {
-                        var tmp = arr.Any(x => x == item.Name);
-                        if(!tmp)
-                        {
-                            Console.WriteLine("Property not exists " + item.Name);
-                        }
-                    }
-                    line01 = arr;
-                    isHeader = true;
-                    continue;
-                }
-                var p = new Product();
-                foreach (var prop in lists)
-                {
-                    var index = Array.FindIndex(line01, x => x == prop.Name);
-                    if(arr[index] != string.Empty)
-                        prop.SetValue(p,Convert.ChangeType(arr[index],prop.PropertyType));
-                }
-                products.Add(p);
-            }
-            Console.WriteLine($"Name    Price   Description   Expired Date");
-            foreach (var item in products)
-            {
-                Console.WriteLine($"{item.ProductName}  {item.Price}    {item.ProductDescription}    {item.ExpiredDate}");
-            }
-            // Read category
+            //var a = CSVHeper.ReadAllLines("Data\\category.csv");
 
+            //List<Category> listCategory = CSVHeper.ReadFromFile<Category>("Data\\category.csv");
+
+            //listCategory.PrintAll();
+
+            List<Product> listProduct = CsvHelper.ReadFromFile<Product>("Data\\product.csv");
+
+            //listProduct.PrintAll();
+
+            listProduct.WriteToFile("Data\\product1.csv").Wait();
+
+
+            Console.ReadKey();
         }
-    }
-    public class Product
-    {
-        public int ProductId { get; set; }
-        public string ProductName { get; set; }
-        public double Price { get; set; }
-        public string ProductDescription { get; set; }
-        public DateTime ExpiredDate { get; set; }
-        public int CategoryId { get; set; }
-        public bool DeletedFlag { get; set; }
     }
 }
